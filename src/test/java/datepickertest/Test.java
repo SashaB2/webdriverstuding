4bb9721794c4e5935b4ee69bcfb16881980d0fc2
SashaB2 <sviridenko1@gmail.com>
2018-07-26 09:51:20 +0300

    implement transform etc to eu time

--- src/test/java/datepickertest/Test.java
+++ src/test/java/datepickertest/Test.java
@@ -1,58 +1,28 @@
-package pages.datepickerpages;
+package datepickertest;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.chrome.ChromeDriver;
 import pages.ParentPage;
+import pages.datepickerpages.BirthDateDemoPage;
 
 import java.util.Date;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class BirthDateDemoPage extends ParentPage{
+public class Test{
 
-    public BirthDateDemoPage(WebDriver driver) {
-        super(driver);
+    public static void main(String[] args) {
+
+
+            System.out.println(getTime("12:00"));
+
     }
 
-    @FindBy(xpath = "//div[contains(text(), 'Your Birth Date')]")
-    WebElement resultDatePickerXpath;
 
-    public String getDatePickerText(){
-        return resultDatePickerXpath.getText();
-    }
+    static String getTime(String time){
 
-    public String getDate(){
-        String getValues = resultDatePickerXpath.getText();
-        String regexDate = "\\d{4}-\\d{2}-\\d{2}";
-        Pattern p = Pattern.compile(regexDate);
-        Matcher m = p.matcher(getValues);
-
-        String dateValue = "";
-        if(m.find()){
-            dateValue = m.group();
-        }
-
-        char [] digits = new char[8];
-        digits[0] = dateValue.charAt(8);
-        digits[1] = dateValue.charAt(9);
-        digits[2] = dateValue.charAt(5);
-        digits[3] = dateValue.charAt(6);
-        digits[4] = dateValue.charAt(0);
-        digits[5] = dateValue.charAt(1);
-        digits[6] = dateValue.charAt(2);
-        digits[7] = dateValue.charAt(3);
-
-        StringBuilder convertedDate = new StringBuilder();
-        for (int i = 0; i < digits.length; i++) {
-            convertedDate.append(Character.toString(digits[i]));
-        }
-
-        return convertedDate.toString();
-    }
-
-    public String getTime(){
-        String getValues = resultDatePickerXpath.getText();
+        String getValues = time;
         String regexTime = "\\d{2}:\\d{2}";
         Pattern p = Pattern.compile(regexTime);
         Matcher m = p.matcher(getValues);
@@ -66,6 +36,11 @@ public class BirthDateDemoPage extends ParentPage{
         int hoursInt = Integer.parseInt(hours);
 
         String minutes = Character.toString(timeValue.charAt(3)) + Character.toString(timeValue.charAt(4));
+
+
+
+
+
         String hoursEtc = "";
         final String am = "AM";
         final String pm = "PM";
@@ -99,5 +74,13 @@ public class BirthDateDemoPage extends ParentPage{
         convertedTime.append(minutes);
         return convertedTime.toString();
     }
+
+
+    void t (){
+
+
+
+    }
+
 
 }
