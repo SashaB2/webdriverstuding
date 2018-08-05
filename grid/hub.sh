44937aa866335e83978438b15debe68b365ab316
SashaB2 <sviridenko1@gmail.com>
2018-08-05 23:32:38 +0300

    fire fox added

---
+++ grid/hub.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+java -jar selenium-server-standalone-3.13.0.jar -role hub -hubConfig hubconfig.json
---
+++ grid/node.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+java -jar selenium-server-standalone-3.13.0.jar -role node -nodeConfig nodeconfig.json
