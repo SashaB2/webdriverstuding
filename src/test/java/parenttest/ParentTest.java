35a049d318c6e56fa8f287ac72a9bd7a6311d6fa
SashaB2 <sviridenko1@gmail.com>
2018-08-05 18:43:49 +0300

    implement the first step of server configuration

--- src/test/java/parenttest/ParentTest.java
+++ src/test/java/parenttest/ParentTest.java
@@ -96,7 +96,7 @@ public class ParentTest {
 
                 //additional setting for Chrome on Linux
 //                if(Global.isPlatform(Platform.LINUX)){
-                    chromeOptions.setBinary("/opt/google/chrome/");
+                    chromeOptions.setBinary(new File("/opt/google/chrome/chrome"));
                     chromeOptions.addArguments("--no-sandbox");
                     chromeOptions.addArguments("--disable-dev-shm-usage");
 //                }
