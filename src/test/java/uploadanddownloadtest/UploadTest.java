8789ff932d94cf2d2a9c2262593979500952a117
SashaB2 <sviridenko1@gmail.com>
2018-08-06 22:11:15 +0300

    refactor

--- src/test/java/uploadanddownloadtest/UploadTest.java
+++ src/test/java/uploadanddownloadtest/UploadTest.java
@@ -12,14 +12,19 @@ public class UploadTest extends ParentTest {
     @Test
     public void validUploadTest(){
         loginPage.loginQuickly();
+        log.trace("Logged");
         mainPage.clickOnPartiallLink("Selenium");
         mainPage.clickOnPartiallLink("File Upload using Sikuli Demo");
 
         //verify if page this page is opened
         checkAcceptanceCriteria("File Upload using Sikuli page does not open", "File Upload using Sikuli", fileUploadSikuliPage.getPageTitle());
+        log.trace("Sikuli page opened");
+
 
         //upload file
         fileUploadSikuliPage.clickOnChooseFileButton();
+        log.trace("Choose button clicked");
+
         fileUploadSikuliPage.handleWithUploadFile();
         checkAcceptanceCriteria("File does not upload", "File Upload Successful", fileUploadSikuliPage.resultOfUploadFile());
 
