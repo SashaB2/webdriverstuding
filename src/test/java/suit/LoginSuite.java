83af1aa9415c776965aa82b92691bbdba30bc178
SashaB2 <sviridenko1@gmail.com>
2018-08-06 09:17:48 +0300

    refactor

--- src/test/java/suit/LoginSuite.java
+++ src/test/java/suit/LoginSuite.java
@@ -7,10 +7,12 @@ import logintest.ValidLoginTest;
 import mainpagetest.MainPageContentTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import uploadanddownloadtest.UploadTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        ValidLoginTest.class,
+//        ValidLoginTest.class,
+        UploadTest.class
 //        LoginPageFunctionalityTest.class,
 //        MainPageContentTest.class,
 //        DragAndDropTest.class,
