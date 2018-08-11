18ce5bda4ec4d7c4b11739b32d0080c5f681ed89
SashaB2 <sviridenko1@gmail.com>
2018-08-11 23:28:15 +0300

    download file on centos

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -8,10 +8,10 @@ public class Test {
     @org.junit.Test
     public void ver() throws IOException {
 
-        Process execution = Runtime.getRuntime().exec("wget -P /home --no-check-certificate http://demo.guru99.com/test/msgr11us.exe");
+        Process execution = Runtime.getRuntime().exec("/usr/bin/wget -P /home --no-check-certificate http://demo.guru99.com/test/msgr11us.exe");
         try {
             Runtime runtime = Runtime.getRuntime();
-            Process process = runtime.exec(new String[]{"/bin/bash", "-c", "wget -P /home --no-check-certificate http://demo.guru99.com/test/msgr11us.exe"});
+            Process process = runtime.exec(new String[]{"/bin/bash", "-c", "/usr/bin/wget -P /home --no-check-certificate http://demo.guru99.com/test/msgr11us.exe"});
             int exitValue = process.waitFor();
             System.out.println("exit value: " + exitValue);
             BufferedReader buf = new BufferedReader(new InputStreamReader(process.getInputStream()));
