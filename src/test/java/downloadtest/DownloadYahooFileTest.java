7bcd415ee42b2777089f90d7fdca84e06b22e430
SashaB2 <sviridenko1@gmail.com>
2018-08-11 23:18:50 +0300

    download file on centos

--- src/test/java/downloadtest/DownloadYahooFileTest.java
+++ src/test/java/downloadtest/DownloadYahooFileTest.java
@@ -10,7 +10,9 @@ import org.openqa.selenium.Platform;
 import pages.Main;
 import parenttest.ParentTest;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class DownloadYahooFileTest extends ParentTest {
 
@@ -32,7 +34,20 @@ public class DownloadYahooFileTest extends ParentTest {
 //        }
 //        log.info(ConfigData.getConfigValue("wgetDownloadFileYahoo"));
         //download file
-        Process execution = Runtime.getRuntime().exec("wget -P /home --no-check-certificate http://demo.guru99.com/test/msgr11us.exe");
+//        Process execution = Runtime.getRuntime().exec("wget -P /home --no-check-certificate http://demo.guru99.com/test/msgr11us.exe");
+        try {
+            Runtime runtime = Runtime.getRuntime();
+            Process process = runtime.exec(new String[] { "/bin/bash", "-c", "wget -P /home --no-check-certificate http://demo.guru99.com/test/msgr11us.exe" });
+            int exitValue = process.waitFor();
+            System.out.println("exit value: " + exitValue);
+            BufferedReader buf = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = "";
+            while ((line = buf.readLine()) != null) {
+                System.out.println("exec response: " + line);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 //        Main.runLinCom();
 //        fileDownloadPage.downloadFileFromYahooPage(ConfigData.getConfigValue("wgetDownloadFileYahoo"));
 //        checkAcceptanceCriteria("File has not been downloaded", fileDownloadPage.doesDownloadedFilePresent(ConfigData.getConfigValue("downloadFileLocation")), true);
